0bec6bc48312ed8af3c8093ba79bd30e2b0aa423
CrossXyz691 <168219549+CrossXyz691@users.noreply.github.com>
2026-06-01 12:08:09 +0530

    Time: 6 ms (0.92%) | Memory: 46.4 MB (51.15%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -5,18 +5,17 @@ class Solution {
         int secondarySum = 0;
 
         for (int i = 0; i < n; i++) {
-            // Primary diagonal condition: row == col
+        
             primarySum += mat[i][i];
 
-            // Secondary diagonal condition: col = n - 1 - row
+            
             secondarySum += mat[i][n - 1 - i];
         }
 
         System.out.println("Sum of Primary Diagonal: " + primarySum);
         System.out.println("Sum of Secondary Diagonal: " + secondarySum);
         
-        // Optional: Total sum of both diagonals combined
-        // If 'n' is odd, the central element is counted twice, so we subtract it once.
+        
         int totalSum = primarySum + secondarySum;
         if (n % 2 != 0) {
             totalSum -= mat[n / 2][n / 2];
