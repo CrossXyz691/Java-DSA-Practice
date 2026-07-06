496a1193ff818dd0c8c6fffe39ba12bf3339a3cd
CrossXyz691 <168219549+CrossXyz691@users.noreply.github.com>
2026-07-07 04:44:08 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (91.87%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -99,19 +99,4 @@ static int binarysearch(int[] nums, int target,int start,int end){
     return -1;
 }
 
-static int dscBinarySearch(int[] nums,int target,int start,int end){
-    while(start<=end){
-        int mid = start + (end-start)/2;
-        if (nums[mid]>target){
-            start=mid+1;
-        }
-        else if (nums[mid]<target) {
-            end=mid-1;
-        }
-        else {
-            return mid;
-        }
-    }
-    return -1;
-}
 }
