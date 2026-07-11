fc4aa82f23651c7b40bd8318618da5da7fa8ab04
CrossXyz691 <168219549+CrossXyz691@users.noreply.github.com>
2026-07-12 00:09:21 +0530

    Time: 245 ms (5.46%) | Memory: 48.9 MB (9.35%) - LeetSync

--- 924-fair-candy-swap/fair-candy-swap.java
+++ 924-fair-candy-swap/fair-candy-swap.java
@@ -1,8 +1,5 @@
 class Solution {
     public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
-        int aliceShare = 0;
-        int bobShare = 0;
-
         int aliceSum = 0;
         for (int i = 0; i < aliceSizes.length; i++) {
             aliceSum+=aliceSizes[i];
@@ -15,9 +12,7 @@ class Solution {
 
         for (int i = 0; i < aliceSizes.length; i++) {
             for (int j = 0; j < bobSizes.length; j++) {
-                int finalAliceSum = aliceSum - aliceSizes[i];
-                int finalBobSum = bobSum - bobSizes[j];
-                if(finalAliceSum - finalBobSum == aliceSizes[i] - bobSizes[j]){
+                if((aliceSum - aliceSizes[i]) - (bobSum - bobSizes[j]) == aliceSizes[i] - bobSizes[j]){
                     return new int[]{aliceSizes[i], bobSizes[j]};
                 }
             }
