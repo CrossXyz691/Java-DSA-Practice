ce2a1edb6f46153ff54c88dd1cf15fe613d0f1fb
CrossXyz691 <168219549+CrossXyz691@users.noreply.github.com>
2026-08-22 00:36:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (57.16%) - LeetSync

--- 476-number-complement/number-complement.java
+++ 476-number-complement/number-complement.java
@@ -1,16 +1,6 @@
 class Solution {
     public int findComplement(int num) {
-        StringBuilder sb = new StringBuilder();
-        while(num > 0){
-            if((num&1) == 1){
-                sb.append(0);
-            }
-            else{
-                sb.append(1);
-            }
-            num >>= 1;
-        }
-
-        return Integer.parseInt(sb.reverse().toString(), 2);
+        int mask = (Integer.highestOneBit(num) << 1) - 1;
+        return (~num) & mask;
     }
 }
