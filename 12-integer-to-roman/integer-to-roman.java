c473ba7c0d1a16af178e0e613ee10f6be7eff0a7
CrossXyz691 <168219549+CrossXyz691@users.noreply.github.com>
2026-09-01 01:38:02 +0530

    Time: 3 ms (99.81%) | Memory: 46.5 MB (42.89%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,42 +1,21 @@
 class Solution {
     public static String intToRoman(int num) {
-        StringBuilder sb = new StringBuilder();
-        String s = String.valueOf(num);
+        int[] nums={1000,900,500,400,
+                    100,90,50,40,
+                    10,9,5,4,1};
+        String[] s={"M","CM","D","CD",
+                    "C","XC","L","XL",
+                    "X","IX","V","IV","I"};
 
-        for (int i = 0; i < s.length(); i++) {
-            int n = s.charAt(i) - '1' + 1;
-            int place = s.length() - i;
-            sb.append(roman(n, place));
+        StringBuilder result=new StringBuilder();
+        for(int i=0;i<nums.length;i++){
+            while(num>=nums[i]){
+                result.append(s[i]);
+                num-=nums[i];
+            }
         }
-
-        return sb.toString();
+        return result.toString();
     }
 
-    static String roman(int n, int place){
-        StringBuilder sb = new StringBuilder();
-        if(n == 4 && place == 1) return "IV";
-        else if(n == 9 && place == 1) return "IX";
-        else if(n == 4 && place == 2) return "XL";
-        else if(n == 9 && place == 2) return "XC";
-        else if(n == 4 && place == 3) return "CD";
-        else if(n == 9 && place == 3) return "CM";
-        else if(n != 4 && n < 5 && place == 1) sb.append("I".repeat(Math.max(0, n)));
-        else if(n != 9 && n >= 5 && n < 10 && place == 1){
-            sb.append("V");
-            sb.append("I".repeat((n - 5)));
-        }
-        else if (n < 5 && place == 2) sb.append("X".repeat(Math.max(0, n)));
-        else if (n != 9 && n >= 5 && n < 10 && place == 2) {
-            sb.append("L");
-            sb.append("X".repeat((n - 5)));
-        }
-        else if (n < 5 && place == 3) sb.append("C".repeat(Math.max(0, n)));
-        else if (n != 9 && n >= 5 && n < 10 && place == 3) {
-            sb.append("D");
-            sb.append("C".repeat((n - 5)));
-        }
-        else if (n < 5 && place == 4) sb.append("M".repeat(Math.max(0, n)));
-
-        return sb.toString();
-    }
+    
 }
